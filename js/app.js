3dcf4b6a396f15128a2a8451d6a925bf80891a09
sujondev <emd26521@gmail.com>
2022-08-22 17:41:02 +0600

    two function write

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// const selectButtons = document.getElementsByClassName('btn')
+// for (const selectButton of selectButtons) {
+//     selectButton.addEventListener('click', function (event) {
+
+//     })
+// }
+function getInputFeildById(inputFeildId) {
+    const getInputFeild = document.getElementById(inputFeildId)
+    const getInputFeildString = getInputFeild.value;
+    const getInputFeildValue = parseFloat(getInputFeildString);
+    return getInputFeildValue
+}
+
+function getElementById(elementId) {
+    const getElementId = document.getElementById(elementId);
+    const elementIdString = getElementId.innerText;
+    const elementIdValue = parseFloat(elementIdString)
+    return elementIdValue
+}
+
+function setValueByid(elementId, value) {
+    const getElementById = document.getElementById(elementId)
+    getElementById.innerText = value;
+}
+
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const newperPlayerAmount = getInputFeildById('per-player')
+    const currentTotalExpense = newperPlayerAmount * 5
+    const prevTotalExpense = getElementById('total-expenses')
+    setValueByid('total-expenses', currentTotalExpense)
+})
+
+
